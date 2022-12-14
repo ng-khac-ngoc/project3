f33f38d924fc84b057af2d25ab2d280be0e4bd8a
ngocnkhe151168 <ngocnkhe151168@fpt.edu.vn>
2022-12-14 16:05:55 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=database-1.cart42mc9pa1.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=bucket-project3
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
---
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appName: 'Udagram',
+  apiHost: 'http://ae11ca324b08c4d6a8100ccdb0cd7921-1471838954.ap-southeast-1.elb.amazonaws.com:8080/api/v0'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
